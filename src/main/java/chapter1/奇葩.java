76c6dd7b4aa3004d586696976944c860d8116982
Nathanael Yang <ny83427@gmail.com>
2019-05-20 17:33:31 -0700

    Ubuntu运行第一章代码问题修正

--- src/main/java/chapter1/奇葩.java
+++ src/main/java/chapter1/奇葩.java
@@ -1,5 +1,7 @@
 package chapter1;
 
+import java.util.Calendar;
+
 /**
  * <pre>
  * 奇葩 means a super weird man in Chinese, some says Donald Trump is a 奇葩
@@ -27,11 +29,14 @@ public class 奇葩 {
      * When a weird man begins to talk, method name in Japanese
      */
     private void 話す() {
-        System.out.printf("大家好！在下%s，今年%d。总有些人说我奇葩，可您倒是说说，我哪里奇葩啦？", this.이름, this.年齡);
+        System.out.printf("大家好！在下%s，今年%d%n" +
+            "总有些人说我奇葩，可您倒是说说，我哪里奇葩啦？%n", this.이름, this.年齡);
     }
 
     public static void main(String[] args) {
-        奇葩 老罗 = new 奇葩(47, "罗永浩");
+        // 老罗生于1972年7月9日
+        int age = Calendar.getInstance().get(Calendar.YEAR) - 1972;
+        奇葩 老罗 = new 奇葩(age, "罗永浩");
         老罗.話す();
     }
 
